46f49d91944f2303cbcef401f02b8327840b6922
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2025-07-27 17:35:18 -0400

    day 2: complete encoding and decoding functions

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
     "os"
     "io"
     "bufio"
+    "bytes"
+    "strconv"
+    "strings"
     "container/heap"
+    "encoding/binary"
 )
 
 type HuffLookupItem struct {
-    char    int32
+    char    uint32
     freq    int
     code    string
     bits    int
@@ -21,7 +25,46 @@ type HuffNode struct {
     Right   *HuffNode
 }
 
+func (hn *HuffNode) AddNode(char uint32, code uint32, bits uint8) {
+    if bits == 0 {
+        hn.value = &HuffLookupItem{
+            char: char,
+        }
+        return
+    }
+
+    direction := code >> (bits - 1)
+    if direction & 1 == 0 {
+        if hn.Left == nil {
+            hn.Left = &HuffNode{}
+        }
+        hn.Left.AddNode(char, code, bits - 1)
+    } else {
+        if hn.Right == nil {
+            hn.Right = &HuffNode{}
+        }
+        hn.Right.AddNode(char, code, bits - 1)
+    }
+}
+
+func (hn *HuffNode) Walker() func(int) (uint32, bool) {
+    t := hn 
+    return func(direction int) (uint32, bool) {
+        if direction == 0 {
+            t = t.Left 
+        } else {
+            t = t.Right
+        }
+
+        if t.value != nil {
+            return t.value.char, true
+        }
+        return 0, false
+    }
+}
+
 type HuffPriorityQueue []*HuffNode
+type TreeTable map[uint32]*HuffLookupItem
 
 func (pq HuffPriorityQueue) Len() int { return len(pq) }
 
@@ -82,18 +125,101 @@ func HuffmanTreeToString(root *HuffNode) {
     
 }
 
-func Encode(store map[int32]int) {
+func ConvertToBin(value uint32) []byte {
+    result := make([]byte, 4)
+    binary.LittleEndian.PutUint32(result, value)
+    return result
+}
+
+func ConvertCodeToBin(value string) []byte {
+    result := make([]byte, 4)
+    v, err := strconv.ParseUint(value, 2, 32)
+    if err != nil {
+        panic(err)
+    }
+    smallV := uint32(v)
+    binary.LittleEndian.PutUint32(result, smallV)
+    return result
+}
+
+func BuildHeader(root *HuffNode, header *bytes.Buffer) {
+    if root == nil {
+        return
+    }
+
+    item := root.value
+
+    if item.char != 0 {
+        // first four bytes: character code
+        _, err := header.Write(ConvertToBin(item.char))
+        if err != nil {
+            panic(err)
+        }
+        // next four bytes: Huffman assigned code
+        _, err = header.Write(ConvertCodeToBin(item.code))
+        if err != nil {
+            panic(err)
+        }
+        // last byte: bits
+        err = header.WriteByte(byte(item.bits))
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    BuildHeader(root.Left, header)
+    BuildHeader(root.Right, header)
+}
+
+func BuildBody(pt TreeTable, bodyContent []string, bodyOutput *bytes.Buffer) {
+    encodedString := ""
+    for _, c := range bodyContent {
+        for _, r := range c {
+            // TODO: FIX THIS TERRIBLE SOLUTION!
+            item := pt[uint32(r)]
+            encodedString += item.code
+            if len(encodedString) % 8 == 0 {
+                for i := 0; i < len(encodedString); i += 8 {
+                    byteString := encodedString[i : i + 8]
+                    val, err := strconv.ParseUint(byteString, 2, 8)
+                    if err != nil {
+                        panic(err)
+                    }
+                    bodyOutput.WriteByte(byte(val))
+                }
+                encodedString = ""
+            }
+        }
+    }
+    // TODO: IS THERE A BETTER WAY THAN PADDING?
+    if len(encodedString) % 8 != 0 {
+        // pad with extra 0s in the last bits of character
+        encodedString += strings.Repeat("0", 8 - (len(encodedString) % 8))
+    }
+    for i := 0; i < len(encodedString); i += 8 {
+        byteString := encodedString[i : i + 8]
+        val, err := strconv.ParseUint(byteString, 2, 8)
+        if err != nil {
+            panic(err)
+        }
+        bodyOutput.WriteByte(byte(val))
+    }
+}
+
+func Encode(store map[uint32]int, body []string, EncodedText *bytes.Buffer) {
     fmt.Println("ENCODING...")
     // =========== Build Huffman Priority Queue
     pq := make(HuffPriorityQueue, len(store))
+    pt := make(TreeTable)
     i := 0
     for k, v := range store {
         val := &HuffLookupItem{
             char:   k,
             freq:   -v,
         }
+        pt[k] = val
         pq[i] = &HuffNode{
-            value:  val,
+            value:  pt[k],
         }
         i++
     }
@@ -116,9 +242,91 @@ func Encode(store map[int32]int) {
         }
         heap.Push(&pq, &newNode)
     }
-    // =========== Build Huffman Tree
     BuildHuffmanTree(pq[0], "", 0)
-    HuffmanTreeToString(pq[0])
+    //HuffmanTreeToString(pq[0])
+
+    var Header bytes.Buffer
+    BuildHeader(pq[0], &Header)
+    //fmt.Println(Header)
+    fmt.Printf("Header size: %d bytes\n", Header.Len())
+
+    var Body bytes.Buffer
+    BuildBody(pt, body, &Body)
+    //fmt.Printf("%f\n", Body)
+    fmt.Printf("Body size: %d bytes\n", Body.Len())
+    
+    EncodedText.Grow(2 + Header.Len() + Body.Len())
+    headerBin := make([]byte, 2)
+    binary.LittleEndian.PutUint16(headerBin, uint16(Header.Len()))
+    EncodedText.Write(headerBin)
+    fmt.Printf("Write 2 bytes of header length to EncodedText\n")
+    n, err := Header.WriteTo(EncodedText)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Write %d bytes of header to EncodedText\n", n)
+    n, err = Body.WriteTo(EncodedText)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Write %d bytes of body to EncodedText\n", n)
+}
+
+func Decode(text *bytes.Buffer) {
+    fmt.Println("Decoding...")
+    // Extract header length
+    headerLenBin := make([]byte, 2)
+    _, err := text.Read(headerLenBin)
+    if err != nil {
+        panic(err)
+    }
+    headerLen := binary.LittleEndian.Uint16(headerLenBin)
+    fmt.Printf("Extracted header length: %d\n", headerLen)
+    // Split text to header and body sections
+    headerBin := make([]byte, headerLen)
+    _, err = text.Read(headerBin)
+    if err != nil {
+        panic(err)
+    }
+    // Build HuffmanTree with extracted header
+    ht := HuffNode{}
+    // character code + Huffman assigned code + bits -> 4 + 4 + 1 = 9 bytes
+    for i := 0; i < len(headerBin); i += 9 {
+        section := headerBin[i : i + 9]
+        char := binary.LittleEndian.Uint32(section[0: 4])
+        code := binary.LittleEndian.Uint32(section[4 : 8])
+        bits := section[8]
+        ht.AddNode(char, code, bits)
+    }
+    // Decode body using built tree above
+    var decodedString []string
+    walk := ht.Walker()
+    for {
+        bodyBin, err := text.ReadByte()
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            panic(err)
+        }
+
+        for i := 7; i >= 0; i-- {
+            bit := (bodyBin >> uint(i)) & 1 
+            v, ok := walk(int(bit))
+            if ok {
+                decodedString = append(decodedString, string(v))
+                walk = ht.Walker()
+            }
+        }
+    }
+    count := 0
+    for _, c := range decodedString {
+        //fmt.Printf("char: %c, size: %d\n", c, len(c)) 
+        count += len(c)
+    }
+    fmt.Printf("Text size: %d bytes\n", count)
+    // fmt.Printf("Total: %d bytes\n", int(headerLen) + count)
+    //fmt.Println(strings.Join(decodedString, ""))
 }
 
 func main() {
@@ -131,7 +339,7 @@ func main() {
     }
     defer file.Close()
 
-    store := make(map[int32]int)
+    store := make(map[uint32]int)
     body := []string{}
     originalSize := 0
 
@@ -146,7 +354,7 @@ func main() {
         body = append(body, line)
         for _, c := range line {
             // fmt.Printf("%c, %T, %b\n", c, c, c)
-            store[c] += 1
+            store[uint32(c)] += 1
         }
         if err == io.EOF {
             break
@@ -158,5 +366,9 @@ func main() {
     // }
     fmt.Printf("Original File size: %d bytes\n", originalSize)
     fmt.Println("======================")
-    Encode(store)
+    var EncodedText bytes.Buffer
+    Encode(store, body, &EncodedText)
+    fmt.Printf("Total: %d bytes\n", EncodedText.Len())
+    fmt.Println("======================")
+    Decode(&EncodedText)
 }
