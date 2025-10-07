ffc3e59350c6c6ca60b717cb59944417a4d7dff7
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2025-10-06 22:32:04 -0400

    use GCS to streamline data uploading
    - Goal: minimize memory footprint and overloading on the manager
    - Next: build character frequency table alongside streaming.

--- client/main.go
+++ client/main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/google/uuid"
 )
 
 const serverURL = "http://127.0.0.1:8080"
 
-type errorResponse struct {
-	Error string `json:"error"`
+type Application struct {
+	GCSClient *storage.Client
+	CTX       *context.Context
+	Bucket    string
 }
 
 type huffmanRequest struct {
@@ -24,25 +27,22 @@ type huffmanRequest struct {
 	FrequencyTable map[string]int `json:"frequency_table"`
 }
 
-func main() {
-	http.HandleFunc("/upload", uploadHandler)
-	fmt.Println("Listening on localhost:8081...")
-	http.ListenAndServe(":8081", nil)
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+func (app *Application) uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		writeError(w, "Only POST method allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// TODO: increase the limit through any means (the whole point of the program)
-	if err := r.ParseMultipartForm(10 << 20); err != nil {
-		writeError(w, "The uploaded file is too big. Please choose a file less than 10 MB", http.StatusBadRequest)
-		return
-	}
+	// set request body size limit
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<30)
 
-	file, _, err := r.FormFile("file")
+	// // TODO: increase the limit through any means (the whole point of the program)
+	// if err := r.ParseMultipartForm(10 << 20); err != nil {
+	// 	writeError(w, "The uploaded file is too big. Please choose a file less than 10 MB", http.StatusBadRequest)
+	// 	return
+	// }
+
+	file, header, err := r.FormFile("file")
 	if err != nil {
 		log.Printf("ERROR: failed to get file from form: %v", err)
 		writeError(w, "Failed to read file: "+err.Error(), http.StatusBadRequest)
@@ -50,12 +50,35 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	frequencyTable, err := calculateFrequencyFromStream(file)
-	if err != nil {
-		log.Printf("ERROR: failed to process file stream: %v", err)
-		writeError(w, "Could not process file content", http.StatusInternalServerError)
+	jobID := uuid.New().String()
+	log.Printf("Processing new job %s for file %s", jobID, header.Filename)
+
+	// create a pipe to simultaneously building char. req. table while streaming content to GCS
+	// pr, pw := io.Pipe()
+
+	ctx, cancel := context.WithTimeout(*app.CTX, time.Second*50)
+	defer cancel()
+
+	originalFilePath := fmt.Sprintf("%s/original_%s", jobID, header.Filename)
+	wc := app.GCSClient.Bucket(app.Bucket).Object(originalFilePath).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		log.Printf("ERROR: failed to stream data to GCS: %v", err)
+		writeError(w, "Failed to upload data to server: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+	if err := wc.Close(); err != nil {
+		log.Printf("ERROR: failed to close data stream to GCS: %v", err)
+		writeError(w, "Failed to upload data to server: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Uploaded %s to GCS", header.Filename)
+
+	// frequencyTable, err := calculateFrequencyFromStream(file)
+	// if err != nil {
+	// 	log.Printf("ERROR: failed to process file stream: %v", err)
+	// 	writeError(w, "Could not process file content", http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// w.Header().Set("Content-Type", "application/json")
 	// w.WriteHeader(http.StatusOK)
@@ -63,40 +86,33 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// 	log.Printf("ERROR: failed to write frequency table JSON response: %v", err)
 	// }
 
-	newUID, err := uuid.NewRandom()
-	if err != nil {
-		log.Printf("ERROR: failed to generate UUID: %v", err)
-		writeError(w, "Failed to generate unique identifier", http.StatusInternalServerError)
-		return
-	}
-
-	ok = buildHuffmanTree(newUID.String(), frequencyTable)
-	if !ok {
-		writeError(w, "Failed to build Huffman Binary Tree", http.StatusInternalServerError)
-		return
-	}
+	// ok = buildHuffmanTree(newUID.String(), frequencyTable)
+	// if !ok {
+	// 	writeError(w, "Failed to build Huffman Binary Tree", http.StatusInternalServerError)
+	// 	return
+	// }
 }
 
-func buildHuffmanTree(UID string, ft map[string]int) bool {
-	request := huffmanRequest{
-		UID:            UID,
-		FrequencyTable: ft,
-	}
-
-	jsonData, err := json.Marshal(request)
-	if err != nil {
-		log.Printf("ERROR: failed to marshal request: %v", err)
-		return false
-	}
-
-	req, err := http.NewRequest(http.MethodPost, serverURL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Printf("ERROR: could not create HTTP request: %v", err)
-		return false
-	}
-	req.Header.Set("Content-Type", "application/json")
-	return true
-}
+// func buildHuffmanTree(UID string, ft map[string]int) bool {
+// 	request := huffmanRequest{
+// 		UID:            UID,
+// 		FrequencyTable: ft,
+// 	}
+//
+// 	jsonData, err := json.Marshal(request)
+// 	if err != nil {
+// 		log.Printf("ERROR: failed to marshal request: %v", err)
+// 		return false
+// 	}
+//
+// 	req, err := http.NewRequest(http.MethodPost, serverURL, bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		log.Printf("ERROR: could not create HTTP request: %v", err)
+// 		return false
+// 	}
+// 	req.Header.Set("Content-Type", "application/json")
+// 	return true
+// }
 
 func calculateFrequencyFromStream(reader io.Reader) (map[string]int, error) {
 	frequency := make(map[string]int)
@@ -120,45 +136,51 @@ func calculateFrequencyFromStream(reader io.Reader) (map[string]int, error) {
 	return frequency, nil
 }
 
-func processFile(path string) (map[rune]int, []string, error) {
-	file, err := os.Open(path)
+// func processFile(path string) (map[rune]int, []string, error) {
+// 	file, err := os.Open(path)
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+// 	defer file.Close()
+//
+// 	freqTable := make(map[rune]int)
+// 	fileContent := []string{}
+//
+// 	fmt.Println("Building frequency table")
+// 	scanner := bufio.NewReader(file)
+// 	for {
+// 		line, err := scanner.ReadString(byte('\n'))
+// 		if err != nil && err != io.EOF {
+// 			return nil, nil, err
+// 		}
+// 		fileContent = append(fileContent, line)
+// 		for _, c := range line {
+// 			freqTable[c]++
+// 		}
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 	}
+//
+// 	return freqTable, fileContent, nil
+// }
+
+func main() {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return nil, nil, err
+		log.Printf("ERROR: Cannot create new client for GCS: %v", err)
+		return
 	}
-	defer file.Close()
+	defer client.Close()
 
-	freqTable := make(map[rune]int)
-	fileContent := []string{}
-
-	fmt.Println("Building frequency table")
-	scanner := bufio.NewReader(file)
-	for {
-		line, err := scanner.ReadString(byte('\n'))
-		if err != nil && err != io.EOF {
-			return nil, nil, err
-		}
-		fileContent = append(fileContent, line)
-		for _, c := range line {
-			freqTable[c]++
-		}
-		if err == io.EOF {
-			break
-		}
+	app := Application{
+		GCSClient: client,
+		CTX:       &ctx,
+		Bucket:    os.Getenv("GCS_BUCKET"),
 	}
 
-	return freqTable, fileContent, nil
-}
-
-func requestUIDFromServer() (string, error) {
-	return "1", nil
-}
-
-func writeError(w http.ResponseWriter, text string, statusCode int) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	response := errorResponse{Error: text}
-	err := json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Printf("ERROR: failed to write error JSON response: %v", err)
-	}
+	http.HandleFunc("/upload", app.uploadHandler)
+	fmt.Println("Listening on localhost:8081...")
+	http.ListenAndServe(":8081", nil)
 }
