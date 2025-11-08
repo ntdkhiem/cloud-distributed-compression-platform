87f79eb707d067eaf352bbdf005e07c69f8d8c9d
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2025-11-08 17:35:58 -0500

    update worker tests to identify EOF

--- internal/worker/main_test.go
+++ internal/worker/main_test.go
@@ -237,8 +237,8 @@ func TestCompressMessageHandler(t *testing.T) {
 
 		actualContentBuff := bytes.NewBuffer(actualContentReader)
 		// verify the size: actualContentBuff ignores EOF so include it back.
-		if len(content) + 1 != actualContentBuff.Len() {
-			t.Errorf("Expected compressed content to have length of %d bytes but got %d bytes", actualContentBuff.Len(), len(content) + 1)
+		if len(content) != actualContentBuff.Len() {
+			t.Errorf("Expected compressed content to have length of %d bytes but got %d bytes", actualContentBuff.Len(), len(content))
 		}
 
 		// TODO: must have a better check for the content here.
@@ -342,8 +342,8 @@ func TestDecompressMessageHandler(t *testing.T) {
 
 		actualContentBuff := bytes.NewBuffer(actualContentReader)
 		// verify the size: actualContentBuff ignores EOF so include it back.
-		if len(content) != actualContentBuff.Len() + 1 {
-			t.Errorf("Expected compressed content to have length of %d bytes but got %d bytes", actualContentBuff.Len() + 1, len(content))
+		if len(content) != actualContentBuff.Len() {
+			t.Errorf("Expected compressed content to have length of %d bytes but got %d bytes", actualContentBuff.Len(), len(content))
 		}
 
 		if string(content)+"\n" == actualContentBuff.String() {
